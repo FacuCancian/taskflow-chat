6019e96958376ec92fd5e84c86c84d98b3033620
Redselig123 <facucancian@yahoo.com.ar>
2026-04-23 20:15:19 -0300

    fix: database connection config

--- backend/src/main/kotlin/com/facundo/Application.kt
+++ backend/src/main/kotlin/com/facundo/Application.kt
@@ -7,9 +7,20 @@ import com.facundo.config.configureSecurity
 import com.facundo.config.configureSerialization
 import com.facundo.config.configureSockets
 import io.ktor.server.application.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
+    val port = System.getenv("PORT")?.toInt() ?: 8080
+
+    embeddedServer(
+        Netty,
+        port = port,
+        host = "0.0.0.0"
+    ) {
+        module()
+    }.start(wait = true)
 }
 
 fun Application.module() {
--- backend/src/main/kotlin/com/facundo/DatabaseFactory.kt
+++ backend/src/main/kotlin/com/facundo/DatabaseFactory.kt
@@ -1,32 +1,49 @@
-package com.facundo
-
 import com.facundo.features.auth.Users
+import com.facundo.features.chat.Messages
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import io.ktor.server.application.*
-import io.ktor.server.config.ApplicationConfig
+import io.ktor.server.config.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
-import com.facundo.features.chat.Messages
+import java.net.URI
 
 object DatabaseFactory {
 
     fun init(config: ApplicationConfig) {
-        val hikariConfig = HikariConfig().apply {
-            jdbcUrl = config.property("database.url").getString()
-            driverClassName = config.property("database.driver").getString()
-            username = config.property("database.user").getString()
-            password = config.property("database.password").getString()
-            maximumPoolSize = 10
+
+        val databaseUrl = System.getenv("DATABASE_URL")
+        val hikariConfig = if (databaseUrl != null) {
+            val uri = URI(databaseUrl)
+
+            val username = uri.userInfo.split(":")[0]
+            val password = uri.userInfo.split(":")[1]
+
+            val jdbcUrl =
+                "jdbc:postgresql://${uri.host}:${uri.port}${uri.path}?sslmode=require"
+
+            HikariConfig().apply {
+                this.jdbcUrl = jdbcUrl
+                this.driverClassName = "org.postgresql.Driver"
+                this.username = username
+                this.password = password
+                this.maximumPoolSize = 10
+            }
+
+        } else {
+            HikariConfig().apply {
+                jdbcUrl = config.property("database.url").getString()
+                driverClassName = config.property("database.driver").getString()
+                username = config.property("database.user").getString()
+                password = config.property("database.password").getString()
+                maximumPoolSize = 10
+            }
         }
 
         val dataSource = HikariDataSource(hikariConfig)
         Database.connect(dataSource)
 
-
         transaction {
-            SchemaUtils.createMissingTablesAndColumns(Users)
             SchemaUtils.createMissingTablesAndColumns(Users, Messages)
         }
     }
