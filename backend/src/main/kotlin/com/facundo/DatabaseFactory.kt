78aabeb865e22cb6e5e8e86c2bcf94881471b128
Redselig123 <facucancian@yahoo.com.ar>
2026-04-23 21:08:45 -0300

    fixed dbFactory

--- backend/src/main/kotlin/com/facundo/DatabaseFactory.kt
+++ backend/src/main/kotlin/com/facundo/DatabaseFactory.kt
@@ -6,38 +6,20 @@ import io.ktor.server.config.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.net.URI
 
 object DatabaseFactory {
 
     fun init(config: ApplicationConfig) {
-
-        val databaseUrl = System.getenv("DATABASE_URL")
-        val hikariConfig = if (databaseUrl != null) {
-            val uri = URI(databaseUrl)
-
-            val username = System.getenv("DATABASE_USER")
-            val password = System.getenv("DATABASE_PASSWORD")
-
-            val jdbcUrl =
-                "jdbc:postgresql://${uri.host}:${uri.port}/${uri.path}?sslmode=require"
-
-            HikariConfig().apply {
-                this.jdbcUrl = databaseUrl
-                this.driverClassName = "org.postgresql.Driver"
-                this.username = username
-                this.password = password
-                this.maximumPoolSize = 10
-            }
-
-        } else {
-            HikariConfig().apply {
-                jdbcUrl = config.property("database.url").getString()
-                driverClassName = config.property("database.driver").getString()
-                username = config.property("database.user").getString()
-                password = config.property("database.password").getString()
-                maximumPoolSize = 10
-            }
+        val hikariConfig = HikariConfig().apply {
+            jdbcUrl = System.getenv("DATABASE_URL")
+                ?: config.property("database.url").getString()
+            driverClassName = "org.postgresql.Driver"
+            username = System.getenv("DATABASE_USER")
+                ?: config.property("database.user").getString()
+            password = System.getenv("DATABASE_PASSWORD")
+                ?: config.property("database.password").getString()
+            maximumPoolSize = 10
+            isAutoCommit = false
         }
 
         val dataSource = HikariDataSource(hikariConfig)
