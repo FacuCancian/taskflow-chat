82f1dd2f5df7adbcfcd00eb51c976ce6d006dbfe
Redselig123 <facucancian@yahoo.com.ar>
2026-05-01 14:46:23 -0300

    change of url for request

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,12 +1,18 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://localhost:8082',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json'
   }
 })
-
+http.interceptors.request.use(config => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
 http.interceptors.response.use(
   response => response,
   error => {
